422aaeb698debfa1cb790a9944a05a6c2ef86ef9
patlehmann1 <patrick.lehmann@techs-MacBook-Pro-2.local>
2020-01-12 22:06:38 -0500

    initial commit, added routes

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/name/<name>')
+def hello_user(name):
+    return 'Hello, %s' % escape(name)
+
+@app.route('/square_a_number/<int:number>')
+def square_a_number(number):
+    squared = number * number
+    return f'{number} squared is {squared}'
